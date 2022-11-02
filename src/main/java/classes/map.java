48bd3bdfbd0347e646a5b86027ec9a9edada9f3c
JAndrei209 <84650632+JAndrei209@users.noreply.github.com>
2022-11-02 16:39:41 -0700

    filled up score_streak and map_java

--- src/main/java/classes/map.java
+++ src/main/java/classes/map.java
@@ -4,6 +4,8 @@ import java.awt.event.ActionListener;
 import javax.swing.AbstractAction;
 import javax.swing.Timer;
 
+import org.apache.commons.math3.ode.events.Action;
+
 import acm.program.GraphicsProgram;
 
 public class map extends GraphicsProgram implements ActionListener{
@@ -14,6 +16,34 @@ public class map extends GraphicsProgram implements ActionListener{
 	Action Aaction;
 	Action Saction;
 	Action Daction;
+	// All map library
+	
+	void failChecker() {
+		//activates when buttons are pressed
+		// looks at current map --> fail screen
+		
+
+	}
+	
+	void passChecker() {
+		//activates when buttons are pressed
+		// looks at current map --> pass screen
+	}
+	void SpawnNotes() {
+		// pulls from conductor
+	}
+	
+	//map.getInputMap().put(Key.Stroke.getKeyStroke("w"), "Waction");
+	//label.getActionMap().put("Waction",Waction);
+	
+	//map.getInputMap().put(Key.Stroke.getKeyStroke("a"), "Aaction");
+	//label.getActionMap().put("Aaction",Aaction);
+	
+	//map.getInputMap().put(Key.Stroke.getKeyStroke("s"), "Saction");
+	//label.getActionMap().put("Saction",Saction);
+	
+	//map.getInputMap().put(Key.Stroke.getKeyStroke("d"), "Daction");
+	//label.getActionMap().put("daction",Daction);
 	
 	
 	public class Waction extends AbstractAction{
---
+++ src/main/java/classes/score_streak.java
@@ -0,0 +1,21 @@
+
+public class score_streak{
+	int streak = 0;
+	int score = 0;
+	int failCount = 0; 
+	void incrementScore() {
+		score ++;
+	}
+	void incrementStreak() {
+		streak ++;
+	}
+	void reset_score(){
+		score =0;
+	}
+	void reset_streak() {
+		streak = 0;
+				
+	}
+	int streak_multiplier;
+	
+}
