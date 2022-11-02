3f9970ca79c686d13278910bc68c3e3bdca8b6d1
Nathan <n_naiditch@u.pacific.edu>
2022-11-02 16:50:26 -0700

    Added a song folder and started to make conductor class

--- src/main/java/classes/conductor.java
+++ src/main/java/classes/conductor.java
@@ -1,5 +1,27 @@
 package classes;
 
 public class conductor {
-
+	public conductor(int bpm, song music, int speed, food notes[]){
+		BPM = bpm;
+		Song = music;
+		reactionTime = speed;
+		mapNotes = notes;
+	}
+	private int BPM;
+	private double currentBeat;
+	private song Song;
+	private int reactionTime;
+	private food mapNotes[];
+	void setBPM(int bpm) {
+		BPM = bpm;
+	}
+	int getBPM(){
+		return BPM;
+	}
+	void stopSong() {
+		
+	}
+	double getCurrentBeat() {
+		return 0;
+	}
 }
