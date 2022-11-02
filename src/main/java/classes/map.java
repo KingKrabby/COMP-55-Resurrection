c5b5184fa70c1dcc9c79380184ef4bb64abdd0d8
JAndrei209 <84650632+JAndrei209@users.noreply.github.com>
2022-11-02 16:13:16 -0700

    Added empty WASD buttons

---
+++ src/main/java/classes/map.java
@@ -0,0 +1,44 @@
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.AbstractAction;
+import javax.swing.Timer;
+
+import acm.program.GraphicsProgram;
+
+public class map extends GraphicsProgram implements ActionListener{
+	Timer t = new Timer(1000,this);
+	// Conveyor
+	// int: current note -- > pulls from conductor
+	public class Waction extends AbstractAction{
+		@Override
+		public void actionPerformed(ActionEvent e) {
+			
+		}
+	}
+	public class Aaction extends AbstractAction{
+		@Override
+		public void actionPerformed(ActionEvent e) {
+			
+		}
+	}
+	public class Saction extends AbstractAction{
+		@Override
+		public void actionPerformed(ActionEvent e) {
+			
+		}
+	}
+	public class Daction extends AbstractAction{
+		@Override
+		public void actionPerformed(ActionEvent e) {
+			
+		}
+	}
+	
+	@Override
+	public void run() {
+		// TODO Auto-generated method stub
+		
+	} 
+
+}
