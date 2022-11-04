d617d006100150d0af6a4b5c12545e24e9570038
Caitlin <c_tan16@u.pacific.edu>
2022-11-04 16:28:50 -0700

    finished FoodType

--- src/main/java/classes/FoodType.java
+++ src/main/java/classes/FoodType.java
@@ -4,5 +4,15 @@ public enum FoodType {
 	
 	BUN, TOMATO, KETCHUP, TOFU;
 	
+	public String toString() {
+		switch(this) {
+			case BUN: return "bun";
+			case TOMATO: return "tomato";
+			case KETCHUP: return "ketchup";
+			case TOFU: return "tofu";
+		}
+		return "n/a";
+	}
+
 
 }
