e6538b902549556acaa87af755b2d19e737394f6
Caitlin <c_tan16@u.pacific.edu>
2022-11-04 16:26:54 -0700

    added to food class

--- src/main/java/classes/food.java
+++ src/main/java/classes/food.java
@@ -1,5 +1,20 @@
 package classes;
 
 public class food {
-	//testing push
+	public FoodType ftype;
+	public double spawnOnBeat;
+	public boolean rightSpawn;
+
+	food(FoodType type, double beat) {
+		ftype = type;
+		spawnOnBeat = beat;
+	}
+	
+	// add public FoodType getFoodType() {
+		
+	
+	public double getSpawnBeat(food currFood) {
+		return spawnOnBeat;
+	}
+
 }
