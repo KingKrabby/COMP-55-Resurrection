848b301f2780f95ec9d0d8b7ef35e7b2553eb1a2
Caitlin <c_tan16@u.pacific.edu>
2022-11-04 16:46:33 -0700

    added to conveyor

--- src/main/java/classes/Conveyor.java
+++ src/main/java/classes/Conveyor.java
@@ -1,6 +1,15 @@
 package classes;
 
 public class Conveyor {
-	
+	public int length;
+	public int noteSpeed;
+	public boolean hardMode;
+
+	Conveyor(int currLength, int currNoteSpeed, boolean currHard) {
+	length = currLength;
+	noteSpeed = currNoteSpeed;
+
+	}
+
 
 }
