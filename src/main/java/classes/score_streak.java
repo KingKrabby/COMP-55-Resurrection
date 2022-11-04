7fad338d50af9d2468a9f6a236c2b2d91880882b
JAndrei209 <84650632+JAndrei209@users.noreply.github.com>
2022-11-04 15:38:28 -0700

    score streak comments

--- src/main/java/classes/score_streak.java
+++ src/main/java/classes/score_streak.java
@@ -1,21 +1,28 @@
 package classes;
 public class score_streak{
-	int streak = 0;
-	int score = 0;
-	int failCount = 0; 
+	int streak = 0; //initializes streak to start at 0
+	
+	int score = 0; // user start at 0 points
+	int failCount = 0; // user starts at 0 fail count
+	
+	// increments score by one for each successive combo
 	void incrementScore() {
 		score ++;
 	}
+	// increments streak by one for each successive combo
 	void incrementStreak() {
 		streak ++;
 	}
+	// score becomes 0 each level
 	void reset_score(){
 		score =0;
 	}
+	// streak resets after failed streak.
 	void reset_streak() {
 		streak = 0;
 				
 	}
+	// 
 	int streak_multiplier;
 	
 }
