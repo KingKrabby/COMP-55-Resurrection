759ef1ee9e180f9eb742ff49e15cd7d933cb8378
Caitlin <c_tan16@u.pacific.edu>
2022-11-06 14:59:31 -0800

    conveyor updated

--- src/main/java/classes/Conveyor.java
+++ src/main/java/classes/Conveyor.java
@@ -1,15 +1,38 @@
 package classes;
+import acm.graphics.*;
+import acm.program.*;
+import acm.util.*;
+import java.awt.*;
+import java.util.*;
 
 public class Conveyor {
 	public int length;
 	public int noteSpeed;
 	public boolean hardMode;
+	int numConveyors;
+	ArrayList<Spawner> spawners = new ArrayList<Spawner>();
+	ArrayList<HitCircle> hitCircles = new ArrayList<HitCircle>();
 
 	Conveyor(int currLength, int currNoteSpeed, boolean currHard) {
-	length = currLength;
-	noteSpeed = currNoteSpeed;
-
+		length = currLength;
+		noteSpeed = currNoteSpeed;
+		numConveyors = 1;
+			for (int i = 0; i < numConveyors; i++) {
+				//?? idk how spawners will work yet
+				//trying to add spawners to arraylist
+				//add hitcircles to arraylist
+			}
+		
+			
 	}
+	
+	public void moveNote() {
+		//get info from spawner to move note
+		//timer?
+		//conductor?
+	}
+	
+	
 
 
 }
