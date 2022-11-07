42879fd124da6e149bd53301de3b1016ea5209fb
Nathan <n_naiditch@u.pacific.edu>
2022-11-06 16:35:56 -0800

    Revert "Added get current beat to conductor, needs a bit of refinement to get beats to be more exact"
    This reverts commit 8e3a09ddd22d52a568752e812939472d83b1e034.

--- src/main/java/classes/Conductor.java
+++ src/main/java/classes/Conductor.java
@@ -1,15 +1,13 @@
 package classes;
-import javafx.util.Duration;
-import java.util.Scanner;
 
 public class Conductor {
-	public Conductor(double bpm, Song music, int speed, Food notes[]){
+	public Conductor(int bpm, Song music, int speed, Food notes[]){
 		BPM = bpm;
 		song = music;
 		reactionTime = speed;
 		mapNotes = notes;
 	}
-	private double BPM;
+	private int BPM;
 	private double currentBeat;
 	private Song song;
 	private int reactionTime;
@@ -17,11 +15,8 @@ public class Conductor {
 	void setBPM(int bpm) {
 		BPM = bpm;
 	}
-	double getBPM(){
+	int getBPM(){
 		return BPM;
-	}
-	void setBPM(double bpm) {
-		BPM = bpm;
 	}
 	void playSong(String level) {
 		song.playSound("songs", level + ".mp3");
@@ -29,26 +24,12 @@ public class Conductor {
 	void stopSong(String level) {
 		song.stopSound("songs", level + ".mp3");
 	}
-	double getCurrentBeat(String level) {
-		Duration songDuration = song.findSound("songs", level + ".mp3").getCurrentTime();
-		double currentPos = (double)songDuration.toSeconds();
-		currentBeat = currentPos * (BPM / 60);
-		return currentBeat;
+	double getCurrentBeat() {
+		return 0;
 	}
 	public static void main(String[] args) {
 		Song testSong = Song.getInstance();
 		Conductor testConductor = new Conductor(150, testSong, 1, null);
-		Scanner scan = new Scanner(System.in);
 		testConductor.playSong("tutorial");
-		for(int i = 0; i < 10; i++) {
-			int input = scan.nextInt();
-			if(input == 1) {
-				testConductor.stopSong("tutorial");
-			}
-			if (input == 2) {
-				System.out.println(testConductor.getCurrentBeat("tutorial"));
-			}
-		}
-		System.exit(0);
 	}
 }
--- src/main/java/classes/Song.java
+++ src/main/java/classes/Song.java
@@ -119,7 +119,7 @@ public final class Song {
 		return resource.toString();
 	}
 
-	public MediaPlayer findSound(String folder, String filename) {
+	private MediaPlayer findSound(String folder, String filename) {
 		return players.get(folder + filename);
 	}
 
