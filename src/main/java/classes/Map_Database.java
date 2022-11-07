30cfa407ab700ea31d21a74e8e3a2bd0ce9c4701
Nathan <n_naiditch@u.pacific.edu>
2022-11-06 19:02:10 -0800

    Added instance variables to Map_Database and Spawner

--- src/main/java/classes/Map_Database.java
+++ src/main/java/classes/Map_Database.java
@@ -1,5 +1,8 @@
 package classes;
 
 public class Map_Database {
-
+	Food tutorial[];
+	Food level1[];
+	Food level2[];
+	Food level3[];
 }
--- src/main/java/classes/Spawner.java
+++ src/main/java/classes/Spawner.java
@@ -2,5 +2,9 @@ package classes;
 
 
 public class Spawner{
-	
+	Boolean hardMode;
+	Conductor tempo;
+	int spawnerX;
+	int spawnerY;
+	void spawnFood(spawnerX, )
 }
