eb8146f758afbceff012bee5d7d88ed4df9b2bd8
Amaru Chavez <c_chavez18@u.pacific.edu>
2022-11-06 18:54:51 -0800

    Added commented plan for fail screen

--- src/main/java/classes/Fail_screen.java
+++ src/main/java/classes/Fail_screen.java
@@ -10,7 +10,12 @@ public class Fail_screen{
 	}
 	
 	public Fail_screen(){
-		
+		//area for creating a pass screen once we start communicating between classes
+		//"Oof, better luck next time" at the top of the screen
+		//Picture of a tofu cartoon being sad at the bottom right
+		//"Press space bar to try again" underneath "Oof, better luck next time"
+		//"Press Enter to return back to the menu" underneath "Oof, better luck next time"
+		//"World's Hardest Games" symbol in the bottom left
 	}
 	
 	public void onSpacePressed(KeyEvent s) {
