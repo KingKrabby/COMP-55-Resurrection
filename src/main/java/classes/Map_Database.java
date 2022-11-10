a875dd9ee4623c62e6587aeae73f1c0c92801727
Nathan <n_naiditch@u.pacific.edu>
2022-11-09 17:13:34 -0800

    Updated map database to be easier to access

--- src/main/java/classes/Map_Database.java
+++ src/main/java/classes/Map_Database.java
@@ -1,13 +1,14 @@
 package classes;
 
-public class Map_Database {
-	Food[] tutorial = new Food[] { new Food(FoodType.BUN, 1), 
+public final class Map_Database {
+	private Map_Database() {}
+	
+	public static Food[] tutorial = new Food[] { new Food(FoodType.BUN, 1), 
 			new Food(FoodType.KETCHUP, 5),
 			new Food(FoodType.TOFU, 9),
 			new Food(FoodType.TOMATO, 13)};
-	Food level1[];
-	Food level2[];
-	Food level3[];
+	public static Food level1[];
+	public static Food level2[];
+	public static Food level3[];
 
 }
-
