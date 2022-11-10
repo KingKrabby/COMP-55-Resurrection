a4174574f51eb3bc505def94d26805939230c8ee
Amaru Chavez <c_chavez18@u.pacific.edu>
2022-11-09 16:13:24 -0800

    GLabels rough draft in Title Screen, commented creating screen size

--- src/main/java/classes/Title_screen.java
+++ src/main/java/classes/Title_screen.java
@@ -1,6 +1,9 @@
 package classes;
+import java.awt.Color;
 import java.awt.event.KeyEvent;
-//import acm.graphics.GObject;
+
+import acm.graphics.GLabel;
+import acm.graphics.GObject;
 
 public class Title_screen {
 	private Play_game program;
@@ -9,16 +12,41 @@ public class Title_screen {
 	int get_identifier() {
 		return identifier;
 	}
+	/*
+	private static final int START_Y = 250;
+	private static final int START_X = 300;
+	public static final int PROGRAM_HEIGHT = 600;
+	public static final int PROGRAM_WIDTH = 800;
 	
+	public void init() {
+		//setSize(PROGRAM_WIDTH, PROGRAM_HEIGHT);
+		//requestFocus();
+	}
+	*/
 	
-	public Title_screen(){
+	public void run(){
+		//Title of Game
+		GLabel x = new GLabel("Turnt Up Tofu!", 20, 50);
+		x.setColor(Color.black);
+		x.setFont("Arial-18");
+		//add(x);
+		
+		GLabel y = new GLabel("Press Spacebar to Start!", 20, 50);
+		y.setColor(Color.black);
+		y.setFont("Arial-18");
+		//add(y);
+		
+		
 		//area for creating a title screen once we start communicating between classes
 		//"Turnt Up Tofu" at the top of the screen
 		//Picture of a record player with a spinning burger bun instead of a vinyl at the center of the screen
 		//"Press space bar to start!" underneath record player
 		//"World's Hardest Games" symbol in the bottom right
 	}
+
 	
+
+
 	public void keyPressed(KeyEvent s) {
 		int keyCode = s.getKeyCode();
 		if (keyCode == KeyEvent.VK_SPACE) {
