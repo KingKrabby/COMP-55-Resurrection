8c27379b87496e50cb43356ec8b0d8b156a984b9
Caitlin <c_tan16@u.pacific.edu>
2022-11-11 16:34:43 -0800

    commented isHit() function in HitCircle

--- src/main/java/classes/HitCircle.java
+++ src/main/java/classes/HitCircle.java
@@ -9,15 +9,29 @@ public class HitCircle {
 			hitCircleY = y;
 	}		
 
-	public boolean isHit(double currentBeat, double spawnOnBeat) { 
+	/* public boolean isHit(Food currentFood) { 
 		//currentBeat will be from conductor and spawnOnBeat will be from Food
+		double currentBeat = currentLevel.getConductor().getCurrentBeat(currentLevel);
+		double spawnOnBeat = currentFood.getSpawnBeat();
 		if ((currentBeat <= (spawnOnBeat + 0.25)) && currentBeat >= (spawnOnBeat - 0.25)) {
 			return true;
 		}
 		else {
 			return false;
 		}
+		
+		/*
+		 in map:
+		 for (int i = 0; i < level1.length(); i++) {
+		 	if (level1[i].isHit == true) {
+		 	
+		 	}
+		 	else {
+		 	
+		 	}
+		 }
+		 
 	}
-
+*/
 	
 }
