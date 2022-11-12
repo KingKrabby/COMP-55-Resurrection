0127038dc417ebbfafc96c072999b1880916d3e2
Nathan <n_naiditch@u.pacific.edu>
2022-11-11 16:41:53 -0800

    Added constructor for spawner

--- src/main/java/classes/Spawner.java
+++ src/main/java/classes/Spawner.java
@@ -1,10 +1,17 @@
 package classes;
+import java.awt.Color;
+import java.awt.event.KeyEvent;
+import acm.program.*;
+import acm.graphics.*;
 
 
 public class Spawner{
+	public Spawner(int x, int y, Boolean hard) {
+		spawnerX = x;
+		spawnerY = y;
+		hardMode = hard;
+	}
 	Boolean hardMode;
-	Conductor tempo;
 	int spawnerX;
-	//int spawnerY;
-	//void spawnFood(spawnerX, )
+	int spawnerY;
 }
