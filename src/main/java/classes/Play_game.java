70109ca149ec0a94803583dfb4e0e54ff85d608f
Amaru Chavez <c_chavez18@u.pacific.edu>
2022-11-13 17:05:51 -0800

    Tested Play_Game; Failed

--- src/main/java/classes/Play_game.java
+++ src/main/java/classes/Play_game.java
@@ -1,18 +1,18 @@
 package classes;
 //The main screen where everything is displayed
-import acm.program.GraphicsProgram;
 
-public class Play_game extends GraphicsProgram{
+public class Play_game extends Map{
 	//screen windows
 	public static final int WINDOW_WIDTH = 800;
 	public static final int WINDOW_HEIGHT = 600;
 	//title screen background music
 	public static final String MUSIC_FOLDER = "songs";
-	private static final String[] SOUND_FILES = { "tutorial.mp3"};
+	private static final String[] SOUND_FILES = { "tutorial.mp3" };
 	
-	//private Menu_screen menu;
-	//private Pass_screen pass;
-	//private Fail_screen fail;
+	private Title_screen title;
+	private Menu_screen menu;
+	private Pass_screen pass;
+	private Fail_screen fail;
 	private int count;
 	
 	public void init() {
@@ -21,14 +21,16 @@ public class Play_game extends GraphicsProgram{
 	
 	public void run() {
 		System.out.println("Turnt Up Tofu!");
-		//menu = new Menu_screen(this);
-		//setupInteractions();
+		title = new Title_screen();
+		menu = new Menu_screen();
+		switchToMenu();
 	}
 	
 	
 	public void switchToMenu() {
 		playBackgroundNoise();
 		count++;
+		int map_track = 0;
 		//switchToScreen(menu);
 	}
 	
