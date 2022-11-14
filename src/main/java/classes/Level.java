7f57ac527002d50af11d55930e414507e0602029
Caitlin <c_tan16@u.pacific.edu>
2022-11-13 16:36:08 -0800

    created variables, constructors, and getters for level class

--- src/main/java/classes/Level.java
+++ src/main/java/classes/Level.java
@@ -2,18 +2,46 @@ package classes;
 
 public class Level{
 	
-	Level() {
-		//initializes a conductor, conveyor array, food array, song, hit circle array, soawner
+	Conductor conductor;
+	Conveyor[] conveyorBelts;
+	Food[] foodItems;
+	Song song;
+	HitCircle[] hitCircles;
+	Spawner spawner;
+	
+	Level(Conductor currConductor, Conveyor[] currBelts, Food[] currItems, Song currSong, HitCircle[] currCircles, Spawner currSpawner) {
+		conductor = currConductor;
+		conveyorBelts = currBelts;
+		foodItems = currItems;
+		song = currSong;
+		hitCircles = currCircles;
+		spawner = currSpawner;
+		
 	}
 
+	public Conductor getConductor() {
+		return conductor;
+	}
 	
+	public Conveyor[] getConveyorBelts() {
+		return conveyorBelts;
+	}
 	
-	//getConductor
-	//getConveyor
-	//getFoodArray
-	//getHitCircleArray
-	//getSpawner
+	public Food[] getFoodItems(){
+		return foodItems;
+	}
 	
+	public Song getSong(){
+		return song;
+	}
+	
+	public HitCircle[] getHitCircles() {
+		return hitCircles;
+	}
+	
+	public Spawner getSpawner() {
+		return spawner;
+	}
 	
 	
 }
