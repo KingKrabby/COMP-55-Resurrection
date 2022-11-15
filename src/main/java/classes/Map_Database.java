c3ed2aefe66119fe8bfc7b8c94cacc6c21eb48bb
ctan18 <ctan18@STK-ECS-JR3N243.stk.pacific.edu>
2022-11-14 16:30:11 -0800

    fixed erros

--- src/main/java/classes/Map_Database.java
+++ src/main/java/classes/Map_Database.java
@@ -32,9 +32,9 @@ public final class Map_Database {
 	public static Spawner spawner3;  //Nate need to complete initialization
 	
 
-	Level level1 = new Level("level1",conductor1, conveyor1, food1, song1, circle1, spawner1);
-	Level level2 = new Level("level2",conductor2, conveyor2, food2, song2, circle2, spawner2);
-	Level level3 = new Level("level3", conductor3, conveyor3, food3, song3, circle3, spawner3);
+	Level level1 = new Level("level1",conductor1, conveyor1, food1, song1, circle1[0], spawner1);
+	Level level2 = new Level("level2",conductor2, conveyor2, food2, song2, circle2[0], spawner2);
+	Level level3 = new Level("level3", conductor3, conveyor3, food3, song3, circle3[0], spawner3);
 
 
 	// get hitcircles
