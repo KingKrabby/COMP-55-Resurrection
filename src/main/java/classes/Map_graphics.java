1536d46096e0a7429f9b83cd43e6dd9264e1777f
JAndrei209 <84650632+JAndrei209@users.noreply.github.com>
2022-11-16 16:04:32 -0800

    Filling up score streak graphics information

--- src/main/java/classes/Map_graphics.java
+++ src/main/java/classes/Map_graphics.java
@@ -1,9 +1,14 @@
 package classes;
 
+import java.awt.event.ActionListener;
 
+import javax.swing.Timer;
 
-public class Map_graphics{
-	// Conductor
+import acm.graphics.GLabel;
+import acm.graphics.GRect;
+
+public class Map_graphics extends GraphicsProgram implements ActionListener{
+	
 	
 	// Conveyor
 	
@@ -17,12 +22,28 @@ public class Map_graphics{
 	
 	
 	// score streak 
+
+		int score_streak_SIZE = 100;
+		int score_streak_loc_x = 400;
+		int score_streak_loc_y = 400;
+		int score_streak_ms = 50;
+		GRect score_streak = new GRect(score_streak_SIZE, score_streak_SIZE, score_streak_loc_x,score_streak_loc_y );
+		Timer score_streak_graphic = new Timer(score_streak_ms, this);
+		GLabel score;
+		GLabel streak;
+		GLabel fail; 
+		
 	
 	// spawner
 	
+	
 	// Food items
-	
-	
+	public void run() {
+		
+	}
+	public static void main(String args[]) {
+		new Map_graphics().start();
+	}
 	
 	
 	
