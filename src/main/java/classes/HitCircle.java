0a142f93b763b15af317f63f2ed11f4152bd1851
Caitlin <c_tan16@u.pacific.edu>
2022-11-16 16:36:08 -0800

    added base code for graphics to HitCircle

--- src/main/java/classes/HitCircle.java
+++ src/main/java/classes/HitCircle.java
@@ -1,8 +1,15 @@
 package classes;
+import acm.program.*;
+import acm.util.*;
+import java.awt.*;
+
 
 public class HitCircle {
 	public int hitCircleX;
 	public int hitCircleY;
+	//int numHitCircles;
+	//need to find way to retrieve hitcircle num from mapdatabase?
+	//or maybe change constructor to have number parameter
 	
 	HitCircle(int x, int y) {
 			hitCircleX = x;
@@ -20,5 +27,21 @@ public class HitCircle {
 			return false;
 		}
 	}
+	
+	/*
+	 //public void setUpHitCircle() {
+	  	if (numHitCircles == 1) {
+	  		GImage singleHitCircle = new GImage();
+	  		add(singleHitCircle);
+	  	}
+	  	if (numHitCircles ==2) {
+	  		GImage hitCircle1 = new GImage();
+	  		add(hitCircle1);
+	  		GImage hitCircle2 = new GImage();
+	  		add(hitCircle2);
+	  	}
+	  
+	  }
+	 */
 	
 }
