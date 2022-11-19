cf13c194dee60f1c4eb152842b393de86f068eb4
Amaru Chavez <c_chavez18@u.pacific.edu>
2022-11-18 16:33:39 -0800

    Added map_track, count and to background music to menu

--- src/main/java/classes/Menu_screen.java
+++ src/main/java/classes/Menu_screen.java
@@ -1,13 +1,24 @@
 package classes;
 import java.awt.event.KeyEvent;
+import acm.graphics.*;
 
-public class Menu_screen {
+public class Menu_screen extends Play_game{
 	// function to identify map 
+	private Play_game program;
 	private int identifier = 1;
 	int get_identifier() {
 		return identifier;
 	}
-	public Menu_screen(){
+	
+	public static final String MUSIC_FOLDER = "songs";
+	private static final String[] SOUND_FILES = { "menu.mp3" };
+	
+	private int count;
+	
+	public Menu_screen(Play_game app){
+		super();
+		program = app;
+		playBackgroundNoise();
 		//area for creating a menu screen once we start communicating between classes
 		//"Turnt Up Tofu" at the top left of the screen
 		//Picture of a tofu cartoon dancing at the middle left
@@ -15,12 +26,15 @@ public class Menu_screen {
 		//"Press Space bar to play the first level!" underneath "Press 'L' to play the Tutorial!"
 		//"Press 'Esc' to quit the game" underneath "Press Space bar to play the first level!"
 		//"World's Hardest Games" symbol in the bottom right
+		
 	}
 	
 	public void onSpacePressed(KeyEvent s) {
 		int keyCode = s.getKeyCode();
 		if (keyCode == KeyEvent.VK_SPACE) {
 			//plays first level
+			count++;
+			map_track = 3;
 		}
 	}
 	
@@ -28,13 +42,22 @@ public class Menu_screen {
 		int keyCode = L.getKeyCode();
 		if (keyCode == KeyEvent.VK_L) {
 			//plays the tutorial
+			count++;
+			map_track = 1;
+			
 		}
 	}
 	
+	@Override
 	public void keyPressed(KeyEvent e) {
 		int keyCode = e.getKeyCode();
 		if (keyCode == KeyEvent.VK_ESCAPE) {
 			//quits the game
 		}
+	}
+	
+	private void playBackgroundNoise() {
+		Song test = Song.getInstance();
+		test.playSound(MUSIC_FOLDER, SOUND_FILES[count % SOUND_FILES.length]);
 	}
 }
