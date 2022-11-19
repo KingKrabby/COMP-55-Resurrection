17753f35fff2698ea6d02b735e6e1e9df964ef1f
Caitlin <c_tan16@u.pacific.edu>
2022-11-18 16:12:18 -0800

    pushed comments in hit circle

--- src/main/java/classes/HitCircle.java
+++ src/main/java/classes/HitCircle.java
@@ -7,6 +7,16 @@ import java.awt.*;
 public class HitCircle {
 	public int hitCircleX;
 	public int hitCircleY;
+	public static final int WINDOW_WIDTH = 800;
+	public static final int WINDOW_HEIGHT = 600;
+	//public static final int x1; will be for HitCircle on large conveyor
+	//public static final int y1; will be for HitCircle on large conveyor
+	//public static final int x2; will be for first HitCircle on smaller conveyors
+	//public static final int y2; will be for first HitCircle on smaller conveyors
+	//public static final int x3; will be for second HitCircle on smaller conveyors
+	//public static final int y3; will be for second HitCircle on smaller conveyors
+	
+	
 	//int numHitCircles;
 	//need to find way to retrieve hitcircle num from mapdatabase?
 	//or maybe change constructor to have number parameter
