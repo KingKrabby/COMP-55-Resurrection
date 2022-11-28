28462c89ffbb46cb53873e7458ea2cc632c35bed
Caitlin <c_tan16@u.pacific.edu>
2022-11-27 18:30:47 -0800

    added GImages for food

--- src/main/java/classes/Food.java
+++ src/main/java/classes/Food.java
@@ -1,13 +1,31 @@
 package classes;
+import acm.graphics.*;
+import acm.program.*;
+import acm.util.*;
+import java.awt.*;
 
 public class Food {
 	public FoodType ftype;
 	public double spawnOnBeat;
 	public boolean rightSpawn;
+	public GImage foodImage;
 
 	Food(FoodType type, double beat) {
 		ftype = type;
 		spawnOnBeat = beat;
+		
+		if (ftype.toString() == "bun") {
+			foodImage = new GImage("bun.png", 0, 0);
+		}
+		if (ftype.toString() == "tomato") {
+			foodImage = new GImage("tomato.png", 0, 0);
+		}
+		if (ftype.toString() == "ketchup") {
+			foodImage = new GImage("ketchup.png", 0, 0);
+		}
+		if (ftype.toString() == "tofu") {
+			foodImage = new GImage("tofu.png", 0, 0);
+		}
 	}
 	
 	public FoodType getFoodType() {
