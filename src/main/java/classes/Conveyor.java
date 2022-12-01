c011f463a8a240b6b843cd2cf5c4e8b1227fba7e
Caitlin <c_tan16@u.pacific.edu>
2022-11-30 16:15:54 -0800

    updated conveyor png and x and y coords

--- src/main/java/classes/Conveyor.java
+++ src/main/java/classes/Conveyor.java
@@ -12,7 +12,7 @@ public class Conveyor extends GraphicsProgram {
 	public static final int WINDOW_WIDTH = 800;
 	public static final int WINDOW_HEIGHT = 600;
 	public static final int x1 = 0; //still need to initialize these
-	public static final int y1 = 0; 
+	public static final int y1 = 500; 
 	public static final int x2 = 0;
 	public static final int y2 = 0; 
 	
@@ -31,7 +31,7 @@ public class Conveyor extends GraphicsProgram {
 	
 	 public void run() {
 	  	if (numConveyors == 1) {
-	  		GImage singleConveyor = new GImage("conveyor1.png", x1, y1);
+	  		GImage singleConveyor = new GImage("longconveyor.png", x1, y1);
 	  		add(singleConveyor);
 	  	}
 	  	
