2447bf0695f375b08e68f46643fa8049326c31fb
JAndrei209 <84650632+JAndrei209@users.noreply.github.com>
2022-12-04 00:02:11 -0800

    Finished Map

--- src/main/java/classes/Map.java
+++ src/main/java/classes/Map.java
@@ -39,62 +39,13 @@ public class Map extends GraphicsProgram implements ActionListener, KeyListener 
 	Level[] level_arr = new Level[] {level_1, level_2, level_3};
 	int curr_level_num = 0;
 	Level current = level_1;
-	
-	boolean fail() {
-		System.out.println("fail");
-		//activates when buttons are pressed
-		// looks at current map --> fail screen
-		if (box.get_failCount() == 3) {
-			box.reset_fail();
-			return true;
-		}
-		return false;
-	}
-	boolean pass(int score, Level level) {
-		System.out.println("pass");
-		if(score == level.get_food_length()) {
-			box.reset();
-			curr_level_num ++;
-			if (curr_level_num < 3) {
-				current = level_arr[curr_level_num];
-			}
-			pass.run();
-			return true;
-		}
-		return false;
-	}
-	//Take Screen to menu screen
+		
 	public void run() { 
 		addKeyListeners();
 		title.start();
 		
 	
-	
 	}
-
-
-	@Override
-	public void keyPressed(KeyEvent e) {
-		System.out.println("hello");
-		int key = e.getKeyCode(); 
-		
-		if (map_track == 2) {
-			System.out.println("hello");
-			pass.run();
-			}
-		
-		
-			
-		
-
-		
-	}
-	
-
-	
-	
-	
-	
 
 	
 	public static void main(String args[]) {
