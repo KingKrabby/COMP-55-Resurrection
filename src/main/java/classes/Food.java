e286c230da19d1bfe6328b986980bd430a185932
Caitlin <c_tan16@u.pacific.edu>
2022-12-04 16:21:53 -0800

    removed excess code

--- src/main/java/classes/Food.java
+++ src/main/java/classes/Food.java
@@ -6,24 +6,11 @@ public class Food {
 	public FoodType ftype;
 	public double spawnOnBeat;
 	public boolean rightSpawn;
-	public GImage foodImage;
 
 	Food(FoodType type, double beat) {
 		ftype = type;
 		spawnOnBeat = beat;
 		
-		if (type == FoodType.BUN) {
-			foodImage = new GImage("bun.png", 0, 0);
-		}
-		if (type == FoodType.TOMATO) {
-			foodImage = new GImage("tomato.png", 0, 0);
-		}
-		if (type == FoodType.KETCHUP) {
-			foodImage = new GImage("ketchup.png", 0, 0);
-		}
-		if (type == FoodType.TOFU) {
-			foodImage = new GImage("tofu.png", 0, 0);
-		}
 	}
 	
 	public FoodType getFoodType() {
