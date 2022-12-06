cc46b952bbf9b0a6a7d70cf4a145c5416ad020c8
JAndrei209 <84650632+JAndrei209@users.noreply.github.com>
2022-12-05 19:04:51 -0800

    Created W

--- src/main/java/classes/tutorial.java
+++ src/main/java/classes/tutorial.java
@@ -2,6 +2,8 @@ package classes;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 import java.util.ArrayList;
 
 import javax.swing.Timer;
@@ -12,16 +14,19 @@ import acm.graphics.GObject;
 import acm.graphics.GOval;
 import acm.program.GraphicsProgram;
 
-public class tutorial extends GraphicsProgram implements ActionListener{
+public class tutorial extends Map{
 	int x_loc = 0;
-	int y_loc = 300;
+	int y_loc = 435;
 	int speed = 20;
 	int track = 0;
 	long start;
 	long end;
 	long elapsed;
 	int count;
+	int count1 = 0;
 	int check;
+	Timer movement;
+	GLabel W;
 	ArrayList<GImage> list = new ArrayList<GImage> ();
 	public void run() {
 
@@ -29,9 +34,11 @@ public class tutorial extends GraphicsProgram implements ActionListener{
 		GImage background = new GImage("title_screen_bluebackground.jpg", 0, 0);
 		
 		GImage hCircle = new GImage("hitcircle.png", 550, 400);
-
+		W = new GLabel("W", 650, 300);
+		W.setFont("Arial-70");
 		
-		Timer movement;
+		
+		
 		final int MS = 1000;
 		
 		count = 0;
@@ -40,8 +47,12 @@ public class tutorial extends GraphicsProgram implements ActionListener{
 		add(background);
 		add(belt);
 		add(hCircle);
+		add(W);
+		
+		
 		movement = new Timer(MS, this);
 		movement.start();
+		addKeyListeners();
 		
 		
 	}
@@ -51,10 +62,20 @@ public class tutorial extends GraphicsProgram implements ActionListener{
 		GImage tofu = new GImage("tofu.png", x_loc, y_loc);
 		GImage tomato = new GImage("tomato.png", x_loc, y_loc);
 		GImage[] food = new GImage[] {bun, ketchup, tofu, tomato};
-		
-		
+		long change = 0, schange, echange;
+		int i = 0;
 		for (GImage f:list) {
 			f.move(speed, 0);
+			if (f.getX() > 600 && count1 == i) {
+				schange = System.currentTimeMillis();
+				while (change < 5000) {
+					echange = System.currentTimeMillis();
+					change = echange - schange;	
+				}
+				count1 ++;
+				
+			}
+			i ++;
 		}
 		end = System.currentTimeMillis();
 		elapsed = end - start;
@@ -62,14 +83,32 @@ public class tutorial extends GraphicsProgram implements ActionListener{
 		if (elapsed > check && count < 4) {
 			list.add(food[count]);
 			add(food[count]);
-			check += 1000;
+			check += 4000;
 			count += 1;
 		}
 		
+	}
+	public void label(int num) {
+		
 		
 	}
-	public void move() {
-
+	
+	@Override
+	public void keyPressed(KeyEvent e) {
+		
+		int keyCode = e.getKeyCode();
+		if (keyCode == KeyEvent.VK_RIGHT) {
+			speed += 5;
+		}
+		if (keyCode == KeyEvent.VK_LEFT) {
+			if (speed > 0) {
+				speed --;
+			}
+			
+		}
+		if (keyCode == KeyEvent.VK_ESCAPE) {
+			menu.start();
+		}
 	}
 	public static void main(String args[]) {
 		new tutorial().start();
